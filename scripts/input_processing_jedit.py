ee27eb94dae1707e01303da471df581e744e81c1
Unknown <guilhermemgadelha@gmail.com>
2018-10-03 22:06:45 -0300

    start bug fixing input_processing_jedit script

--- scripts/input_processing_jedit.py
+++ scripts/input_processing_jedit.py
@@ -1,11 +1,7 @@
 
 import pandas as pd
 import codecs
-
-files = ['BR_UC']
-
-trace_dict = {}
-df_trace = None
+import os
 
 sources_names = []
 targets_names = []
@@ -13,7 +9,6 @@ targets_names = []
 df_artifacts_descriptions = pd.DataFrame(columns=['artf_name','artf_description'])
 
 BASE_DIR = 'data/jEdit/jEditDataset/'
-
 
 def _register_source_artifact(s_name):
     if s_name not in sources_names:
@@ -43,7 +38,7 @@ def _register_target_artifact(t_list_names):
 
     for t_name in t_list_names:
         if t_name not in targets_names:
-            targets_names.append(t_name)
+            targets_ahnames.append(t_name)
 
             print("t_name: " + t_name)
 
@@ -64,42 +59,63 @@ def _register_target_artifact(t_list_names):
                     df_artifacts_descriptions = df_artifacts_descriptions.append(new_row, sort=False)
 
 
-def read_oracle_files():
-    for f_name in files:
-        with open(BASE_DIR + 'oracle/' + f_name + '.txt', 'r') as fl:
-            lines = fl.readlines()
-            source_abv = f_name.split('_')[0] + '_'
-            target_abv = f_name.split('_')[1] + '_'
-            for l in lines:
-                source, targets = l.split(':')[0], l.split(':')[1]
-                targets_list = targets.split(' ')
-                
-                s_name = source_abv + source.replace('.txt', '') + '_SRC'
-                t_list_names = [target_abv  + t.replace('.txt', '').replace('\n','') + '_TRG' for t in targets_list]
 
-                trace_dict[s_name] = t_list_names
+class OracleLoader():
 
-                _register_source_artifact(s_name)
-                _register_target_artifact(t_list_names)
+    def __init__(self):
+        self.files = ['BR_UC']
+        self.trace_dict = {}
+        self.df_trace = None
 
+        for f_name in self.files:
+            with open(BASE_DIR + 'oracle/' + f_name + '.txt', 'r') as fl:
+                lines = fl.readlines()
+                source_abv = f_name.split('_')[0] + '_'
+                target_abv = f_name.split('_')[1] + '_'
+                for l in lines:
+                    source, targets = l.split(':')[0], l.split(':')[1]
+                    targets_list = targets.split(' ')
+                    
+                    s_name = source_abv + source.replace('.txt', '') + '_SRC'
+                    t_list_names = [target_abv  + t.replace('.txt', '').replace('\n','') + '_TRG' for t in targets_list]
 
-def dict2df():
-    rows_list = []
-    for artf_1 in sources_names:
-        for artf_2 in targets_names:
-            if artf_2.split('_')[2] == 'TRG' and artf_1.split('_')[2] == 'SRC':
-                if artf_2 in trace_dict[artf_1]:
-                    rows_list.append({'artf_1': artf_1, 'artf_2':artf_2, 'link': 1})
-                else:
-                    rows_list.append({'artf_1': artf_1, 'artf_2':artf_2, 'link': 0})
+                    self.trace_dict[s_name] = t_list_names
+
+                    #_register_source_artifact(s_name)
+                    #_register_target_artifact(t_list_names)
+
+    def get_trace_value(self, src_art_name, trg_art_name):
+        if src_art_name in self.trace_dict.keys():
+            if trg_art_name in self.trace_dict[src_art_name].value():
+                return 1
+            else: 
+                return 0
+        return 0
     
-    global df_trace
-    df_trace = pd.DataFrame(rows_list)
+    def _get_list_of_target_artifacts(self):
+        
+        return 
 
 
-if __name__ == '__main__':
-    read_oracle_files()
-    dict2df()
+    def save_oracle_as_csv(self):
+        list_source_artifacts = os.listdir(BASE_DIR + "/docs_english/BR/")
+        list_target_artifacts = os.listdir(BASE_DIR + "/docs_english/UC/")
 
-    df_trace.to_csv(BASE_DIR + '/oracle/output/trace_matrix.csv', sep=',', index=False)
-    df_artifacts_descriptions.to_csv(BASE_DIR + '/oracle/output/artifacts_descriptions.csv', sep=',', index=False)
+        rows_list = []
+        for artf_1 in list_source_artifacts:
+            for artf_2 in list_target_artifacts:
+                if artf_2.split('_')[2] == 'TRG' and artf_1.split('_')[2] == 'SRC':
+                    if artf_2 in self.trace_dict[artf_1]:
+                        rows_list.append({'src_artf': artf_1, 'trg_artf':artf_2, 'link': 1})
+                    else:
+                        rows_list.append({'src_artf': artf_1, 'trg_artf':artf_2, 'link': 0})
+        
+        self.df_trace = pd.DataFrame(rows_list)
+        self.df_trace.to_csv(BASE_DIR + '/oracle/output/trace_matrix.csv', sep=',', index=False)
+
+
+if __name__ == '__main__':    
+    oLoader = OracleLoader()
+    oLoader.save_oracle_as_csv()
+   
+    #df_artifacts_descriptions.to_csv(BASE_DIR + '/oracle/output/artifacts_descriptions.csv', sep=',', index=False)
