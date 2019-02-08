2f92c34d6a77fd1d6613a1228fe35d1c0d92c8a1
Unknown <guilhermemgadelha@gmail.com>
2019-02-08 16:15:00 -0300

    download additional info of bug reports and start preprocessing it

--- data/mozilla_firefox_v2/firefoxDataset/docs_english/scripts/get_all_bugs_add_info.py
+++ data/mozilla_firefox_v2/firefoxDataset/docs_english/scripts/get_all_bugs_add_info.py
@@ -7,14 +7,14 @@ PRODUCT = "firefox"
 QUERY_FORMAT = "advanced"
 QUERY_BASED_ON = ""
 J_TOP = "OR"
-LIMIT = 50
+LIMIT = 500
 
 BASE_URL = "https://bugzilla.mozilla.org/rest/bug?chfieldfrom={}&chfieldto={}&product={}&query_format={}&limit={}&query_based_on={}&j_top={}&offset={}"
 BASE_URL_COMMENT = "https://bugzilla.mozilla.org/rest/bug/"
-BUGS_FILE_PATH = '/home/guilherme/anaconda3/envs/trace-link-recovery-study/data/mozilla_firefox/firefoxDataset/docs_english/BR_2/all_bugs_p3.csv'
+BUGS_FILE_PATH = '/home/guilherme/anaconda3/envs/trace-link-recovery-study/data/mozilla_firefox_v2/firefoxDataset/docs_english/BR/all_bugs_add_info.csv'
 
-header = "Bug_Number|Summary|Platform|Component|Version|Creation_Time|Whiteboard|QA_Whiteboard|First_Comment_Text|First_Comment_Creation_Time\n"
-line = "{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}|{9}\n"
+header = "Bug_Number|Status|Product|Priority|Resolution|Severity|Is_Confirmed\n"
+line = "{0}|{1}|{2}|{3}|{4}|{5}|{6}\n"
 
 args = [INIT_DATE, END_DATE, PRODUCT, QUERY_FORMAT, LIMIT, QUERY_BASED_ON, J_TOP]
 
@@ -23,7 +23,7 @@ if not os.path.exists(BUGS_FILE_PATH):
     f.write(header)
     f.close()
     
-for offset_ in range(37000, 37750, LIMIT): # 0, 7000, 1000
+for offset_ in range(0, 37750, LIMIT): # 0, 7000, 1000
     if len(args) < 8:
         args = args + [offset_]
     else:
@@ -37,23 +37,17 @@ for offset_ in range(37000, 37750, LIMIT): # 0, 7000, 1000
     for bug in bugs_json['bugs']:
         bug_id = bug['id']
                 
-        bug_first_comment = requests.get(BASE_URL_COMMENT + str(bug_id) + '/comment')
-        bug_fcomment = bug_first_comment.json()
-
         print("Bug Id: " + str(bug_id))
 
         with open(BUGS_FILE_PATH, 'a+') as bug_file:
             bug_file.write(line.format(
                 bug['id'],
-                bug['summary'],
-                bug['platform'],
-                bug['component'],
-                bug['version'],
-                bug['creation_time'],
-                bug['whiteboard'],
-                bug['cf_qa_whiteboard'],
-                bug_fcomment['bugs'][str(bug_id)]['comments'][0]['text'].replace("\n"," "),
-                bug_fcomment['bugs'][str(bug_id)]['comments'][0]['creation_time']
+                bug['status'],
+                bug['product'],
+                bug['priority'],
+                bug['resolution'],
+                bug['severity'],
+                bug['is_confirmed']
             )
         )
         
--- notebooks/firefox/utils/firefox_dataset.py
+++ notebooks/firefox/utils/firefox_dataset.py
@@ -1,11 +1,16 @@
 import pandas as pd
 
 def read_trace_df():
-    return pd.read_csv('../../data/mozilla_firefox_v2/firefoxDataset/oracle/output/trace_matrix_final.csv')
+    oracle_df = pd.read_csv('../../data/mozilla_firefox_v2/firefoxDataset/oracle/output/trace_matrix_final.csv')
+    print('Oracle.shape: {}'.format(oracle_df.shape))
+    return oracle_df
     
-
 def read_testcases_df():
-    return pd.read_csv('../../data/mozilla_firefox_v2/firefoxDataset/docs_english/TC/testcases_final.csv')
+    testcases_df = pd.read_csv('../../data/mozilla_firefox_v2/firefoxDataset/docs_english/TC/testcases_final.csv')
+    print('TestCases.shape: {}'.format(testcases_df.shape))
+    return testcases_df
 
 def read_bugreports_df():
-    return pd.read_csv('../../data/mozilla_firefox_v2/firefoxDataset/docs_english/BR/bugreports_final.csv')
+    bugreports_df = pd.read_csv('../../data/mozilla_firefox_v2/firefoxDataset/docs_english/BR/bugreports_final.csv')
+    print('BugReports.shape: {}'.format(bugreports_df.shape))
+    return bugreports_df
