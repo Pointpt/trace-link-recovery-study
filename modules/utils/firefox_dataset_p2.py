ed2cca6dd0f380a3beeb6e7fcdfa91cada685f75
Unknown <guilhermemgadelha@gmail.com>
2019-02-27 18:13:50 -0300

    answers of volunteer 7

--- modules/utils/firefox_dataset_p2.py
+++ modules/utils/firefox_dataset_p2.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 BASE_PATH = '/home/guilherme/anaconda3/envs/trace-link-recovery-study'
 
+# ORACLES ----------------
+
 def read_trace_df():
     oracle_df = pd.read_csv(BASE_PATH + '/data/mozilla_firefox_v2/firefoxDataset/oracle/output/firefox_v2/trace_matrix_final_emp_study.csv')
     oracle_df.set_index('tc_name', inplace=True, drop=True)
@@ -15,6 +17,9 @@ def read_feat_br_trace_df():
     feat_br_trace_df.set_index('feat_name', inplace=True)
     print('Feat_BR_Trace.shape: {}'.format(feat_br_trace_df.shape))
     return feat_br_trace_df
+
+
+# DATASETS: TESTCASES, BUGREPORTS, FEATURES (FORMATTED) -----------
 
 def read_testcases_df():
     testcases_df = pd.read_csv(BASE_PATH + '/data/mozilla_firefox_v2/firefoxDataset/docs_english/TC/testcases_final.csv')
@@ -31,10 +36,21 @@ def read_features_df():
     print('Features.shape: {}'.format(features_df.shape))
     return features_df
 
+
+# ORIGINAL DATASETS: FEATURES, BUGREPORTS (NOT FORMATTED) ---------
+
 def read_orig_features_df():
     orig_features_df = pd.read_csv(BASE_PATH + '/data/mozilla_firefox_v2/firefoxDataset/docs_english/Features/features.csv')
     print('OrigFeatures.shape: {}'.format(orig_features_df.shape))
     return orig_features_df
+
+def read_orig_bugreports_df():
+    orig_bugreports_df = pd.read_csv(BASE_PATH + '/data/mozilla_firefox_v2/firefoxDataset/docs_english/BR/bugreports_final.csv')
+    print('OrigBugReports.shape: {}'.format(orig_bugreports_df.shape))
+    return orig_bugreports_df
+
+
+# EXPERT AND VOLUNTEERS TRACE MATRICES ---------------
 
 def read_expert_matrix_df():
     expert_matrix = pd.read_csv(BASE_PATH + '/data/mozilla_firefox_v2/firefoxDataset/br_feat_recovery_empirical_study/pybossa-apps/recover_taskruns/br_2_feature_matrix_expert.csv')
