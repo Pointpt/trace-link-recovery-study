c1c41b92f091cd2247276bb523115191b49b2474
Unknown <guilhermemgadelha@gmail.com>
2019-03-04 12:41:10 -0300

    oracle feat_br and tc_br with last participant answers compiled and preprocessed

---
+++ data/mozilla_firefox_v2/firefoxDataset/br_feat_recovery_empirical_study/pybossa-apps/recover_taskruns/get_answers_volunteers_2.py
@@ -0,0 +1,82 @@
+# get answers from the last volunteer that contributed through the auxiliary application (project_id=9)
+
+import csv
+import requests
+import os
+
+PYB_PROJECT_ID = 9
+
+taskruns_url = "http://localhost:8081/api/taskrun?project_id={0}".format(PYB_PROJECT_ID)
+
+FIRST_TASK_ID = 1918
+LAST_TASK_ID = 1927
+RANGE_TASK_IDS = range(FIRST_TASK_ID, LAST_TASK_ID+1, 1)
+
+FIELDS = ['bug_number',
+            'new_awesome_bar',
+            'windows_child_mode',
+            'apz_async_scrolling',
+            'browser_customization',
+            'pdf_viewer',
+            'context_menu',
+            'w10_comp', 
+            'tts_in_desktop', 
+            'tts_in_rm', 
+            'webgl_comp',
+            'video_and_canvas_render', 
+            'pointer_lock_api',
+            'webm_eme', 
+            'zoom_indicator',
+            'downloads_dropmaker',
+            'webgl2', 
+            'flac_support', 
+            'indicator_device_perm',
+            'flash_support',  
+            'notificationbox',          
+            'update_directory']
+
+csv_line = "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21}" + "\n"
+
+header = csv_line.format(*FIELDS)
+
+# file with the answers for each task
+BR_2_FEATURE_FILE_PATH_2 = 'mozilla_firefox_v2/firefoxDataset/oracle/output/firefox_v2/feat_br/br_2_feature_matrix_volunteers_2.csv'
+
+if os.path.exists(BR_2_FEATURE_FILE_PATH_2):
+    os.remove(BR_2_FEATURE_FILE_PATH_2)
+
+with open(BR_2_FEATURE_FILE_PATH_2, 'w') as br_2_feature_file:
+    br_2_feature_file.write(header)
+
+
+with open(BR_2_FEATURE_FILE_PATH_2, 'a') as br_2_feature_file:
+    for task_id in RANGE_TASK_IDS:
+        taskruns = requests.get(taskruns_url + "&task_id={0}".format(task_id))
+        taskruns_json = taskruns.json()
+        print("Task Id: " + str(task_id))
+
+        for tr in taskruns_json:
+            br_2_feature_file.write(csv_line.format(
+                                            tr['info']['bug_id'],
+                                            tr['info']['links']['new_awesome_bar'],
+                                            tr['info']['links']['windows_child_mode'],
+                                            tr['info']['links']['apz_async_scrolling'],
+                                            tr['info']['links']['browser_customization'],
+                                            tr['info']['links']['pdf_viewer'],
+                                            tr['info']['links']['context_menu'],
+                                            tr['info']['links']['w10_comp'],
+                                            tr["info"]['links']['tts_in_desktop'],
+                                            tr['info']['links']['tts_in_rm'],
+                                            tr['info']['links']['webgl_comp'],
+                                            tr['info']['links']['video_and_canvas_render'],
+                                            tr["info"]['links']['pointer_lock_api'],
+                                            tr['info']['links']['webm_eme'],
+                                            tr['info']['links']['zoom_indicator'],
+                                            tr['info']['links']['downloads_dropmaker'],
+                                            tr["info"]['links']['webgl2'],
+                                            tr["info"]['links']['flac_support'],
+                                            tr["info"]['links']['indicator_device_perm'],
+                                            tr["info"]['links']['flash_support'],
+                                            tr["info"]['links']['notificationbox'],
+                                            tr["info"]['links']['update_directory'])
+                                    )                                      
