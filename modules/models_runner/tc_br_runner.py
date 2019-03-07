b00c09d1fa95a5384eb08b8ee08bee8714ddde9a
Unknown <guilhermemgadelha@gmail.com>
2019-03-06 21:11:18 -0300

    tc_br_models_runner refactoring

--- modules/models_runner/tc_br_runner.py
+++ modules/models_runner/tc_br_runner.py
@@ -20,8 +20,49 @@ from modules.models.model_hyperps import BM25_Model_Hyperp
 from modules.models.model_hyperps import WordVec_Model_Hyperp
 
 
+class TC_BR_Models_Hyperp:
+    
+    @staticmethod
+    def get_lsi_model_hyperp():
+        return {
+            LSI_Model_Hyperp.SIM_MEASURE_MIN_THRESHOLD.value : ('cosine' , .80),
+            LSI_Model_Hyperp.TOP.value : 15,
+            LSI_Model_Hyperp.SVD_MODEL_N_COMPONENTS.value: 100,
+            LSI_Model_Hyperp.VECTORIZER_NGRAM_RANGE.value: (1,1),
+            LSI_Model_Hyperp.VECTORIZER.value : TfidfVectorizer(stop_words='english', use_idf=True, smooth_idf=True),
+            LSI_Model_Hyperp.VECTORIZER_TOKENIZER.value : tok.WordNetBased_LemmaTokenizer()
+        }
+    
+    @staticmethod
+    def get_lda_model_hyperp():
+        return {
+            LDA_Model_Hyperp.TOP.value : 15,
+            LDA_Model_Hyperp.SIM_MEASURE_MIN_THRESHOLD.value : ('cosine',.75),
+            LDA_Model_Hyperp.LDA_MODEL_N_COMPONENTS.value: 20,
+            LDA_Model_Hyperp.LDA_MODEL_RANDOM_STATE.value : 2,
+            LDA_Model_Hyperp.VECTORIZER_NGRAM_RANGE.value: (1,1),
+            LDA_Model_Hyperp.VECTORIZER.value : TfidfVectorizer(stop_words='english', use_idf=True, smooth_idf=True),
+            LDA_Model_Hyperp.VECTORIZER_TOKENIZER.value : tok.PorterStemmerBased_Tokenizer() 
+        }
+    
+    @staticmethod
+    def get_bm25_model_hyperp():
+        return {
+            BM25_Model_Hyperp.TOP.value : 15,
+            BM25_Model_Hyperp.SIM_MEASURE_MIN_THRESHOLD.value : ('-', 0.0),
+            BM25_Model_Hyperp.TOKENIZER.value : tok.PorterStemmerBased_Tokenizer()
+        }
+    
+    @staticmethod
+    def get_w2v_model_hyperp():
+        return {
+            WordVec_Model_Hyperp.SIM_MEASURE_MIN_THRESHOLD.value : ('cosine', .80),
+            WordVec_Model_Hyperp.TOP.value : 15,
+            WordVec_Model_Hyperp.TOKENIZER.value : tok.PorterStemmerBased_Tokenizer()
+        }
+
 class TC_BR_Runner:
-    def __init__(self):
+    def __init__(self, oracle):
         self.test_cases_df = fd.Datasets.read_testcases_df()
         self.bug_reports_df = fd.Datasets.read_selected_bugreports_df()
         
@@ -31,18 +72,13 @@ class TC_BR_Runner:
         self.test_cases_names = self.test_cases_df.tc_name
         self.bug_reports_names = self.bug_reports_df.br_name
 
-        self.orc = fd.Tc_BR_Oracles.read_oracle_expert_volunteers_intersec_df()
+        self.orc = oracle
+        #fd.Tc_BR_Oracles.read_oracle_expert_volunteers_intersec_df()
 
 
-    def run_lsi_model(self):
-        lsi_hyperp = {
-            LSI_Model_Hyperp.SIM_MEASURE_MIN_THRESHOLD.value : ('cosine' , .80),
-            LSI_Model_Hyperp.TOP.value : 15,
-            LSI_Model_Hyperp.SVD_MODEL_N_COMPONENTS.value: 100,
-            LSI_Model_Hyperp.VECTORIZER_NGRAM_RANGE.value: (1,1),
-            LSI_Model_Hyperp.VECTORIZER.value : TfidfVectorizer(stop_words='english', use_idf=True, smooth_idf=True),
-            LSI_Model_Hyperp.VECTORIZER_TOKENIZER.value : tok.WordNetBased_LemmaTokenizer()
-        }
+    def run_lsi_model(self, lsi_hyperp=None, verbose=False):
+        if lsi_hyperp == None:
+            lsi_hyperp = TC_BR_Models_Hyperp.get_lsi_model_hyperp()
 
         lsi_model = LSI(**lsi_hyperp)
         lsi_model.set_name('LSI_Model_TC_BR')
@@ -50,20 +86,13 @@ class TC_BR_Runner:
 
         print("\nModel Evaluation -------------------------------------------")
         evaluator = m_eval.ModelEvaluator(self.orc, lsi_model)
-        evaluator.evaluate_model(verbose=True)
+        evaluator.evaluate_model(verbose=verbose)
         
         return (lsi_model, evaluator)
     
-    def run_lda_model(self):
-        lda_hyperp = {
-            LDA_Model_Hyperp.TOP.value : 15,
-            LDA_Model_Hyperp.SIM_MEASURE_MIN_THRESHOLD.value : ('cosine',.75),
-            LDA_Model_Hyperp.LDA_MODEL_N_COMPONENTS.value: 20,
-            LDA_Model_Hyperp.LDA_MODEL_RANDOM_STATE.value : 2,
-            LDA_Model_Hyperp.VECTORIZER_NGRAM_RANGE.value: (1,1),
-            LDA_Model_Hyperp.VECTORIZER.value : TfidfVectorizer(stop_words='english', use_idf=True, smooth_idf=True),
-            LDA_Model_Hyperp.VECTORIZER_TOKENIZER.value : tok.PorterStemmerBased_Tokenizer() 
-        }
+    def run_lda_model(self, lda_hyperp=None, verbose=False):
+        if lda_hyperp == None:
+            lda_hyperp = TC_BR_Models_Hyperp.get_lda_model_hyperp()
 
         lda_model = LDA(**lda_hyperp)
         lda_model.set_name('LDA_Model_TC_BR')
@@ -71,16 +100,13 @@ class TC_BR_Runner:
 
         print("\nModel Evaluation -------------------------------------------")
         evaluator = m_eval.ModelEvaluator(self.orc, lda_model)
-        evaluator.evaluate_model(verbose=True)
+        evaluator.evaluate_model(verbose=verbose)
         
         return (lda_model, evaluator)
     
-    def run_bm25_model(self):
-        bm25_hyperp = {
-            BM25_Model_Hyperp.TOP.value : 15,
-            BM25_Model_Hyperp.SIM_MEASURE_MIN_THRESHOLD.value : ('-', 0.0),
-            BM25_Model_Hyperp.TOKENIZER.value : tok.PorterStemmerBased_Tokenizer()
-        }
+    def run_bm25_model(self, bm25_hyperp=None, verbose=False):
+        if bm25_hyperp == None:
+            bm25_hyperp = TC_BR_Models_Hyperp.get_bm25_model_hyperp()
 
         bm25_model = BM_25(**bm25_hyperp)
         bm25_model.set_name('BM25_Model_TC_BR')
@@ -88,16 +114,13 @@ class TC_BR_Runner:
 
         print("\nModel Evaluation -------------------------------------------")
         evaluator = m_eval.ModelEvaluator(self.orc, bm25_model)
-        evaluator.evaluate_model(verbose=True)
+        evaluator.evaluate_model(verbose=verbose)
         
         return (bm25_model, evaluator)
     
-    def run_word2vec_model(self):
-        wv_hyperp = {
-            WordVec_Model_Hyperp.SIM_MEASURE_MIN_THRESHOLD.value : ('cosine', .80),
-            WordVec_Model_Hyperp.TOP.value : 15,
-            WordVec_Model_Hyperp.TOKENIZER.value : tok.PorterStemmerBased_Tokenizer()
-        }
+    def run_word2vec_model(self, wv_hyperp=None, verbose=False):
+        if wv_hyperp == None:
+            wv_hyperp = TC_BR_Models_Hyperp.get_w2v_model_hyperp()
 
         wv_model = WordVec_BasedModel(**wv_hyperp)
         wv_model.set_name('WordVec_Model_TC_BR')
@@ -105,6 +128,6 @@ class TC_BR_Runner:
 
         print("\nModel Evaluation -------------------------------------------")
         evaluator = m_eval.ModelEvaluator(self.orc, wv_model)
-        evaluator.evaluate_model(verbose=True)
+        evaluator.evaluate_model(verbose=verbose)
         
         return (wv_model, evaluator)
