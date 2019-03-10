00c2514698b7d8d7f5d5f6a47d208232b4c97709
Unknown <guilhermemgadelha@gmail.com>
2019-03-10 15:48:39 -0300

    feat_r_runner notebook refactoring finished

--- modules/utils/model_evaluator.py
+++ modules/utils/model_evaluator.py
@@ -59,6 +59,8 @@ class ModelEvaluator:
     
     
     def run_evaluator(self, verbose=False, file=None, model=None, top_values=[1,3,5,10], sim_thresholds=[(sm.SimilarityMeasure.COSINE, 0.0)]):        
+        print("Evaluating {} Model ----- ".format(model.get_model_gen_name().upper()))
+        
         for top_value in top_values:
             for s_name,s_threshold in sim_thresholds:
                 ref_name = "top_{}_{}_{}".format(top_value, s_name.value, s_threshold)
@@ -84,22 +86,51 @@ class ModelEvaluator:
         plt.show()
     
     
-    def plot_evaluations(title):  ## TODO
-        f, (ax1,ax2,ax3) = plt.subplots(1,3, figsize=(20,5))
+    def plot_evaluations(self, title):
+        results = self.get_evaluations_df()
+
+        start_pos, width = 0.25, 0.25
+        pos_1 = list([start_pos,         start_pos+2,         start_pos+4,         start_pos+6]) 
+        pos_2 = list([start_pos+width,   start_pos+2+width,   start_pos+4+width,   start_pos+6+width]) 
+        pos_3 = list([start_pos+2*width, start_pos+2+2*width, start_pos+4+2*width, start_pos+6+2*width]) 
+        pos_4 = list([start_pos+3*width, start_pos+2+3*width, start_pos+4+3*width, start_pos+6+3*width])                
+
+        f, (ax1,ax2,ax3) = plt.subplots(3,1, figsize=(20,15))
         f.suptitle(title)
 
-        model_names = [m.to_upper() for m in self.evals.model.values]
+        model_names = [m.upper() for m in results.model.unique()]
 
         ax1.set_title('Percentual Precision')
-        ax1.bar(model_names, self.evals.precision_perc, color='blue')
+        ax1.bar(pos_1, width=width, height=results[results.model == 'lsi'].perc_precision.values, color='blue')
+        ax1.bar(pos_2, width=width, height=results[results.model == 'lda'].perc_precision.values, color='red')
+        ax1.bar(pos_3, width=width, height=results[results.model == 'bm25'].perc_precision.values, color='green')
+        ax1.bar(pos_4, width=width, height=results[results.model == 'wordvector'].perc_precision.values, color='orange')
         ax1.set(xlabel='model', ylabel='precision')
+        ax1.set_xticks([0.6, 2.6, 4.6, 6.6])
+        ax1.set_xticklabels(model_names)
+        ax1.legend(['TOP 1 - COS 0.0', 'TOP 3 - COS 0.0', 'TOP 5 - COS 0.0', 'TOP 10 - COS 0.0'], loc='upper right')
+        ax1.grid()
 
         ax2.set_title('Percentual Recall')
-        ax2.bar(model_names, self.evals.recall_perc, color='red')
+        ax2.bar(pos_1, width=width, height=results[results.model == 'lsi'].perc_recall, color='blue')
+        ax2.bar(pos_2, width=width, height=results[results.model == 'lda'].perc_recall, color='red')
+        ax2.bar(pos_3, width=width, height=results[results.model == 'bm25'].perc_recall, color='green')
+        ax2.bar(pos_4, width=width, height=results[results.model == 'wordvector'].perc_recall, color='orange')
         ax2.set(xlabel='model', ylabel='recall')
+        ax2.set_xticks([0.6, 2.6, 4.6, 6.6])
+        ax2.set_xticklabels(model_names)
+        ax2.legend(['TOP 1 - COS 0.0', 'TOP 3 - COS 0.0', 'TOP 5 - COS 0.0', 'TOP 10 - COS 0.0'], loc='upper left')
+        ax2.grid()
 
         ax3.set_title('Percentual FScore')
-        ax3.bar(model_names, self.evals.fscore_perc, color='green')
+        ax3.bar(pos_1, width=width, height=results[results.model == 'lsi'].perc_fscore, color='blue')
+        ax3.bar(pos_2, width=width, height=results[results.model == 'lda'].perc_fscore, color='red')
+        ax3.bar(pos_3, width=width, height=results[results.model == 'bm25'].perc_fscore, color='green')
+        ax3.bar(pos_4, width=width, height=results[results.model == 'wordvector'].perc_fscore, color='orange')
         ax3.set(xlabel='model', ylabel='fscore')
+        ax3.set_xticks([0.6, 2.6, 4.6, 6.6])
+        ax3.set_xticklabels(model_names)
+        ax3.legend(['TOP 1 - COS 0.0', 'TOP 3 - COS 0.0', 'TOP 5 - COS 0.0', 'TOP 10 - COS 0.0'], loc='upper right')
+        ax3.grid()
 
         
