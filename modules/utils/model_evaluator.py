8b57053f33bcbf6a4b018ff0354e811f04fc9df5
Unknown <guilhermemgadelha@gmail.com>
2019-03-13 22:53:48 -0300

    init of plots precision, recall and fscore for multiple top values and similarity threshold (lsi model)

--- modules/utils/model_evaluator.py
+++ modules/utils/model_evaluator.py
@@ -63,7 +63,7 @@ class ModelEvaluator:
         
         return {'model':model.get_model_gen_name(), 'ref_name':ref_name, 'perc_precision':round(mean_precision,4)*100, 
                 'perc_recall':round(mean_recall,4)*100, 'perc_fscore':round(mean_fscore,4)*100,
-                'trace_links_df' : self.trace_links_df}
+                'trace_links_df' : self.trace_links_df, 'top':top_value, 'sim_threshold':sim_threshold}
     
     
     def run_evaluator(self, verbose=False, file=None, model=None, top_values=[1,3,5,10], sim_thresholds=[(sm.SimilarityMeasure.COSINE, 0.0)]):        
