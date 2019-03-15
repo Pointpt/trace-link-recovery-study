fixing of calculation of precision, recall and fscore in model_evaluator

modules/utils/model_evaluator.py
@@ -6,7 +6,7 @@ import math
 import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix
 
-from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 from modules.utils import similarity_measures as sm
 
@@ -34,25 +34,16 @@ class ModelEvaluator:
 
         return trace_links_df
     
-    def evaluate_model(self, verbose=False, file=None, model=None, top_value=None, sim_threshold=None, ref_name=""):
-        #print("\n {} Evaluation - {}".format(model.get_name(), ref_name))
-        
+    def evaluate_model(self, verbose=False, file=None, model=None, top_value=None, sim_threshold=None, ref_name=""):        
         self.trace_links_df = self.__fillUp_traceLinksDf(model=model, top_n=top_value, sim_threshold=sim_threshold)
-        
-        y_true = csr_matrix(self.oracle.values, dtype='int8')
-        y_pred = csr_matrix(self.trace_links_df.values, dtype='int8')
-        
-        p, r, f, sp = precision_recall_fscore_support(y_true, y_pred)
-
-        eval_df = pd.DataFrame(columns=['precision','recall','fscore','support'])
-        
-        i = 0
-        for idx, row in self.oracle.iteritems():
-            eval_df.at[idx, 'precision'] = p[i]
-            eval_df.at[idx, 'recall'] = r[i]
-            eval_df.at[idx, 'fscore'] = f[i]
-            eval_df.at[idx, 'support'] = sp[i]
-            i += 1
+                       
+        eval_df = pd.DataFrame(columns=['precision','recall','fscore'],
+                              index=self.oracle.columns)
+        eval_df['precision'] = [precision_score(y_true=self.oracle[col],y_pred=self.trace_links_df[col]) for col in self.oracle.columns]
+        eval_df['recall'] = [recall_score(y_true=self.oracle[col],y_pred=self.trace_links_df[col]) for col in self.oracle.columns]
+        eval_df['fscore'] = [f1_score(y_true=self.oracle[col],y_pred=self.trace_links_df[col]) for col in self.oracle.columns]
+        eval_df.index.name = 'Bug_Number'
+        eval_df.index = eval_df.index.astype(str)       
         
         mean_precision = eval_df.precision.mean()
         mean_recall = eval_df.recall.mean()
@@ -61,9 +52,15 @@ class ModelEvaluator:
         if verbose:
             self.print_report(file)
         
-        return {'model':model.get_model_gen_name(), 'ref_name':ref_name, 'perc_precision':round(mean_precision,4)*100, 
-                'perc_recall':round(mean_recall,4)*100, 'perc_fscore':round(mean_fscore,4)*100,
-                'trace_links_df' : self.trace_links_df, 'top':top_value, 'sim_threshold':sim_threshold}
+        return {'model':model.get_model_gen_name(), 
+                'ref_name':ref_name, 
+                'perc_precision': round(mean_precision,4)*100, 
+                'perc_recall': round(mean_recall,4)*100,
+                'perc_fscore': round(mean_fscore,4)*100,
+                'trace_links_df' : self.trace_links_df, 
+                'top':top_value, 
+                'sim_threshold':sim_threshold,
+                'eval_df':eval_df}
     
     
     def run_evaluator(self, verbose=False, file=None, model=None, top_values=[1,3,5,10], sim_thresholds=[(sm.SimilarityMeasure.COSINE, 0.0)]):        