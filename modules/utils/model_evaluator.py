c6e8d1d44bb26379d541b472d0aa3de3fa1a5be2
guilherme <guilhermemgadelha@gmail.com>
2019-05-02 18:33:23 -0300

    small fixes

--- modules/utils/model_evaluator.py
+++ modules/utils/model_evaluator.py
@@ -195,6 +195,7 @@ class ModelEvaluator:
             ax.set_ylabel('precision')
             ax.set_title(models_names[i].upper() + ' Evaluation')
             ax.set_ylim(0,100)
+            ax.set_xlim(0,60)
 
     # plot precision x recall graph in a single figure for all the models
     def plot_evaluations_5(self, results):
